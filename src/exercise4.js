067c50e1e389b47c9cfe7367cb899b9f18665fbf
ErwannGui <erwann.guillevic@ynov.com>
2017-12-19 15:54:53 +0100

    exos 4 et 5

--- src/exercise4.js
+++ src/exercise4.js
@@ -3,8 +3,6 @@ exports.run = function(input) {
 	function myfunction(a, b, c) {
 		return a*b;
 	}
-	
-	console.log(myFunction, 10, 3);
 
 	/** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
 	return input(myFunction, 10, 3);
--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,9 @@
 exports.run = function(input) {
-  console.log("Exercise #5");
+	var file = input.split("[ \n]");
+	var somme = 0;
+	for (var i=0; i < file.length; i++) {
+		somme += Number(file[i]);
+	}
+	return somme;
+	console.log(somme);
 };
